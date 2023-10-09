9264bb568f3640d321b899a41bb0074cfd54c1fd
mouthree <2115932130@qq.com>
2023-10-09 19:22:34 +0800

    y

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(ap: i32) -> i32{
+    if ap > 40{
+        ap
+    } else {
+        ap * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
